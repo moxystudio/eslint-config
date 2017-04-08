0e3ccc481549393212cf538a16911e46dc2b45a9
André Cruz <andremiguelcruz@msn.com>
2017-04-08 10:47:25 +0100

    Add separate jsdoc test file.

--- test/fixtures/rules/errors/errors.js
+++ test/fixtures/rules/errors/errors.js
@@ -355,30 +355,7 @@ if (Math.random().isNan()) {
 
 // `valid-jsdoc` - enforce valid JSDoc comments
 // ---------------------------------------------------------------------
-// Bad
-(function () {
-    /**
-     * Add two numbers.
-     * @param {number} num The first number.
-     * @returns The sum of the two numbers.
-     */
-    function add(num1, num2) {
-        return num1 + num2;
-    }
-})();
-// Good
-(function () {
-    /**
-     * Add two numbers.
-     *
-     * @param {Number} num1 - The first number.
-     * @param {Number} num2 - The second number.
-     * @returns {Number} The sum of the two numbers.
-     */
-    function add(num1, num2) {
-        return num1 + num2;
-    }
-})();
+// See file ../jsdoc/jsdoc.js
 
 // `valid-typeof` - enforce comparing typeof expressions against valid strings
 // ---------------------------------------------------------------------
---
+++ test/fixtures/rules/jsdoc/jsdoc.js
@@ -0,0 +1,25 @@
+// `valid-jsdoc` - enforce valid JSDoc comments
+// ---------------------------------------------------------------------
+// Bad
+(function () {
+    /**
+     * Add two numbers.
+     * @param {number} num The first number.
+     * @returns The sum of the two numbers.
+     */
+    function add(num1, num2) {
+        return num1 + num2;
+    }
+})();
+// Good
+(function () {
+    /**
+     * Add two numbers.
+     * @param {Number} num1 - The first number.
+     * @param {Number} num2 - The second number.
+     * @returns {Number} The sum of the two numbers.
+     */
+    function add(num1, num2) {
+        return num1 + num2;
+    }
+})();
