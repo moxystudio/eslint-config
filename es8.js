61e0ba174edce6f1830d0e39dd66b9b45219d38e
Rui Neves <rui.neves@moxy.studio>
2017-04-10 18:12:44 +0100

    Add restricted properties rule to es7.

--- es8.js
+++ es8.js
@@ -8,13 +8,4 @@ module.exports = {
         'ecmaVersion': 8,
         'sourceType': 'module',
     },
-    'rules': {
-        // Disallow certain properties on certain objects
-        // comment: if this rule on rules/best-practices is modified, this one needs to be updated too
-        'no-restricted-properties': [2, {
-            'object': 'Math',
-            'property': 'pow',
-            'message': 'Use the exponentiation operator (**) instead.',
-        }],
-    },
 };
