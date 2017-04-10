aeeed326ebec854771bdd1e0cc03e7d224f9b3eb
André Cruz <andremiguelcruz@msn.com>
2017-04-10 16:40:16 +0100

    Unnecessary.

--- rules/node.js
+++ rules/node.js
@@ -11,7 +11,8 @@ module.exports = {
         // Enforces error handling in callbacks
         'handle-callback-err': 1,
         // Disallow require calls to be mixed with regular variable declarations
-        'no-mixed-requires': [2, { 'grouping': true }],
+        // comment: this rule only works per var and we disallow multiple variables per var, so it's disabled
+        'no-mixed-requires': 0,
         // Disallow new operators with calls to require
         'no-new-require': 2,
         // Disallow string concatenation with __dirname and __filename
