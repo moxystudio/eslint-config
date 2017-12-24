6b95eab41bde627dea5bcad8c261a9745cf7876d
André Cruz <andremiguelcruz@msn.com>
2017-12-24 14:11:08 +0000

    test: use snapshot testing

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,36 +2,24 @@
 
 'use strict';
 
+const path = require('path');
 const eslint = require('eslint');
 
-it('should pass on all files', () => {
-    const cli = new eslint.CLIEngine({
-        useEslintrc: true,
-    });
+const cli = new eslint.CLIEngine({
+    useEslintrc: true,
+});
 
-    const report = cli.executeOnFiles([`${__dirname}/fixtures/**/*.js`]);
-    const results = report.results;
+const report = cli.executeOnFiles([`${__dirname}/fixtures/**/*.js`]);
+const results = report.results;
 
-    results.forEach((object) => {
-        const filePath = object.filePath;
+results.forEach((object) => {
+    const filePath = object.filePath;
 
-        // Build actual
-        const actual = object.messages
+    it(`should pass on ${path.relative(__dirname, filePath)}`, () => {
+        const result = object.messages
         .map((message) => ({ rule: message.ruleId, severity: message.severity, line: message.line, column: message.column }))
         .sort((warn1, warn2) => warn1.line - warn2.line || warn1.column - warn2.column || warn1.rule.localeCompare(warn2.rule));
 
-        // Uncomment line below to rewrite all expected json results
-        // require('fs').writeFileSync(filePath.replace(/\.js$/, '.json'), JSON.stringify(actual, null, 4));
-
-        // Read expected
-        const expected = require(filePath.replace(/\.js$/, '.json'));  // eslint-disable-line global-require
-
-        try {
-            expect(actual).toEqual(expected);
-        } catch (err) {
-            console.log(`Tests failed at ${filePath}`);
-            console.log(`You might need to edit ${filePath.replace(/\.js$/, '.json')}`);
-            throw err;
-        }
+        expect(result).toMatchSnapshot();
     });
 });
