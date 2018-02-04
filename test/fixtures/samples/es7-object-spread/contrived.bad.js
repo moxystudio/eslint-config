23c89c66b6bada6d06ccbd2393cb65fc900aa5a5
AfonsoVReis <a.reis_05@hotmail.com>
2018-02-04 23:31:24 +0000

    feat: prefer object spread instead of object assign
    BREAKING CHANGE: code that doesn't use spread will not pass linting
    
    Closes issue #30

---
+++ test/fixtures/samples/es7-object-spread/contrived.bad.js
@@ -0,0 +1,9 @@
+// Made up sample.. not genuine
+
+'use strict';
+
+const foo = {};
+
+const bar = Object.assign({}, foo);
+
+console.log(bar);
--- test/fixtures/samples/es7-object-spread/contrived.good.js
+++ test/fixtures/samples/es7-object-spread/contrived.good.js
@@ -8,3 +8,9 @@ const { foo, ...other } = myObject;
 
 console.log('foo', foo);
 console.log('other properties', other);
+
+// -------------------------------------------------
+
+const a = { ...foo };
+
+Object.assign(a, { d: 2 });
