1e7e5c1cd159e11239c343578b75fa92854a3bd1
André Cruz <andremiguelcruz@msn.com>
2018-02-06 13:53:47 +0000

    fix: fix invalid `prefer-import/prefer-import-over-require`

--- addons/es6-modules.js
+++ addons/es6-modules.js
@@ -16,6 +16,6 @@ module.exports = {
         'global-require': 0,
         // Rule to warn the use of import when the es6-modules addon is activated.
         // comment: This should be a warning instead of an error because there are still cases when we must use require.
-        'prefer-import/prefer-import-over-require': [1, 'warning'],
+        'prefer-import/prefer-import-over-require': 1,
     },
 };
