6ace192db7eeb974972d02e8f80c4251a97938cb
André Costa Lima <andreclima.pt@gmail.com>
2019-08-10 16:40:36 +0100

    feat: add eslint-plugin-react-hooks
    BREAKING CHANGE: Drop support for Node v6

--- addons/react.js
+++ addons/react.js
@@ -3,9 +3,11 @@
 module.exports = {
     'extends': [
         '../rules/react',
+        '../rules/react-hooks',
     ].map(require.resolve),
     'plugins': [
         'react',
+        'react-hooks',
     ],
     'parserOptions': {
         'ecmaFeatures': {
