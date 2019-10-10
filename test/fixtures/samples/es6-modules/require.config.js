932d9db4b086ac8c0bd7d05f3c005e2284c91f59
João Silva <joao.silva@moxy.studio>
2019-10-10 16:07:46 +0100

    chore: added testing, updated documentation

---
+++ test/fixtures/samples/es6-modules/require.config.js
@@ -0,0 +1,7 @@
+// Made up sample.. not genuine
+
+// [...].config.js file, should not warn against require
+
+const test = require('good');
+
+console.log(test);
---
+++ test/fixtures/samples/es6-modules/require.js
@@ -0,0 +1,7 @@
+// Made up sample.. not genuine
+
+// Common file, should warn against require
+
+const test = require('bad');
+
+console.log(test);
