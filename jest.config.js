513bf08971aa2e1f797176c3e045723c4b240afe
André Cruz <andremiguelcruz@msn.com>
2020-03-20 10:48:28 +0000

    chore: use MOXY jest-config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 'use strict';
 
-module.exports = {
-    testMatch: ['**/index.test.js'],
-};
+const { compose, baseConfig } = require('@moxy/jest-config');
+
+module.exports = compose([
+    baseConfig,
+    (config) => {
+        config.testEnvironment = 'node';
+        config.testMatch = ['**/index.test.js'];
+
+        return config;
+    },
+]);
