dc7040d87292961f226bb5b64f6e164884ec2b73
Rita de Oliveira <28600773+ritazoliveira@users.noreply.github.com>
2020-03-30 18:23:39 +0100

    feat: add import order rules (#92)
    BREAKING CHANGE: added rules that enforce import order

--- packages/eslint-config-base/lib/modules/esm.js
+++ packages/eslint-config-base/lib/modules/esm.js
@@ -9,5 +9,13 @@ module.exports = {
         'import/no-amd': 'error',
         // Report CommonJS require calls and module.exports or exports.*
         'import/no-commonjs': ['error', { allowConditionalRequire: false }],
+        // Order imports
+        'import/order': ['error', {
+            groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        }],
+        // Imports should be at top of the module
+        'import/first': 'error',
+        // Last import sould have a newline after
+        'import/newline-after-import': ['error', { count: 1 }],
     },
 };
--- packages/eslint-config-base/test/samples/es2015-esm/contrived.bad.js
+++ packages/eslint-config-base/test/samples/es2015-esm/contrived.bad.js
@@ -1,11 +1,28 @@
 // Made up sample.. not genuine
-
 // Test unnecessary `use strict`
 
 'use strict';
+
+// Test imports with different groups and order
+import fs from 'fs';
+import index from './';
+import foobar from '../../foobar';
+import foo from 'foo';
+import barbaz from './bar/baz';
+import baz from '../baz';
+fs.read(foo);
+
+foobar();
+baz();
+barbaz();
+index();
 
 // Test warning when using require
 
 const bar = require('foo/bar');
 
+// Test imports in body of module
+import biz from '../biz';
+
 bar();
+biz();
