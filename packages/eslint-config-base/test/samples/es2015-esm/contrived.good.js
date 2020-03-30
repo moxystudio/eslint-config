408f17e6310d8861d77b3ce79fae1049c0bf9f69
Rita <ritazoliveira22@gmail.com>
2020-03-30 12:35:23 +0100

    feat: add import order rules
    BREAKING CHANGE: added rules to order import

--- packages/eslint-config-base/test/samples/es2015-esm/contrived.good.js
+++ packages/eslint-config-base/test/samples/es2015-esm/contrived.good.js
@@ -1,9 +1,19 @@
 // Made up sample.. not genuine
 
 import fs from 'fs';
+import foo from 'foo';
+import foobar from '../../foobar';
+import baz from '../baz';
+import bar from './bar/baz';
+import index from './';
 
 fs.readFileSync('/path/to/file');
 
-import bar from 'foo/bar';
+foo();
+
+foobar();
+baz();
 
 bar();
+
+index();
